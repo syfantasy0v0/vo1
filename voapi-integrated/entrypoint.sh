071536fcb0a2781e8c12fd8bb34f8b94e9d2f3ae
syfantasy0v0 <sgpsonnet@gmail.com>
2025-10-05 15:19:13 +0800

    Update entrypoint.sh

--- voapi-integrated/entrypoint.sh
+++ voapi-integrated/entrypoint.sh
@@ -4,22 +4,17 @@
 if [ -z "$(ls -A /var/lib/mysql)" ]; then
     echo "Database directory is empty. Initializing MariaDB..."
     # Initialize MariaDB data directory
-    mysql_install_db --user=mysql --datadir=/var/lib/mysql
+    mariadb-install-db --user=mysql --datadir=/var/lib/mysql
 
     # Start mysqld temporarily to initialize users and databases
-    /usr/bin/mysqld --user=mysql --datadir=/var/lib/mysql --skip-networking &
+    /usr/bin/mariadbd --user=mysql --datadir=/var/lib/mysql --skip-networking &
     pid="$!"
     
     # Wait for it to start
     sleep 10
 
     # Execute initialization SQL
-    mysql -u root < /docker-entrypoint-initdb.d/init-db.sql
-    
-    # Create user specifically for localhost
-    mysql -u root -e "CREATE USER 'voapi'@'localhost' IDENTIFIED BY 'voapi123';"
-    mysql -u root -e "GRANT ALL PRIVILEGES ON \`voapi%\`.* TO 'voapi'@'localhost';"
-    mysql -u root -e "FLUSH PRIVILEGES;"
+    mariadb -u root < /docker-entrypoint-initdb.d/init-db.sql
 
     # Stop the temporary server
     kill "$pid"
